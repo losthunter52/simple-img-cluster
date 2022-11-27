83aaeccaa0bb16105f680b1569f572a781018b82
Maher Daoud <maherdaoud52@gmail.com>
2022-11-27 16:48:33 -0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'coloque_uma_secret_key_aqui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
